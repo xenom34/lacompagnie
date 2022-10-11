97b527dddd354da43a38acb4f79f6299a7a80a6e
lucilefreyssac <lucilefr@outlook.fr>
2022-10-11 14:09:33 +0200

    mise a jour

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -4,7 +4,68 @@ import React, {useCallback} from "react";
 class Features extends React.Component<any, any>{
     render() {
         return (
+            <div className="website-slider-item bg-transparent" id="features" data-navbar-slide="navbar-dark">
+                <div className="website-slider-item-inner">
 
+                    <div className="container text-white">
+                        <div className="row">
+                            <div className="col-12 col-xl-5 mb-7 mb-xl-0">
+                                <h2 className="h1">Features</h2>
+                                <div className="divider bg-white"></div>
+                                <p>Fusce massa dolor, mattis sed ultrices ut, placerat ut leo. Donec sed fringilla lectus, non vulputate orci. Integer id libero euismod, interdum ligula vel, porttitor magna. Sed euismod maximus finibus. Pellentesque tempus ultricies nisi at cursus.</p>
+                            </div>
+                            <div className="col-12 col-xl-6 offset-xl-1">
+                                <div className="row">
+
+                                    <div className="col-md-6 col-sm-6 mb-7">
+                                        <div className="icon-7x mb-4">
+                                            <i className="ti-timer"></i>
+                                        </div>
+                                        <h3 className="h5 mb-0">Launch Quickly</h3>
+                                    </div>
+
+                                    <div className="col-md-6 col-sm-6 mb-7">
+                                        <div className="micon-7x mb-4">
+                                            <i className="ti-brush-alt"></i>
+                                        </div>
+                                        <h3 className="h5 mb-0">Stylish Design</h3>
+                                    </div>
+
+                                    <div className="col-md-6 col-sm-6 mb-7">
+                                        <div className="icon-7x mb-4">
+                                            <i className="ti-book"></i>
+                                        </div>
+                                        <h3 className="h5 mb-0">Well Documented</h3>
+                                    </div>
+
+                                    <div className="col-md-6 col-sm-6 mb-7">
+                                        <div className="icon-7x mb-4">
+                                            <i className="ti-layers"></i>
+                                        </div>
+                                        <h3 className="h5 mb-0">Multiple Styles</h3>
+                                    </div>
+
+                                    <div className="col-md-6 col-sm-6 mb-7 mb-sm-0">
+                                        <div className="icon-7x mb-4">
+                                            <i className="ti-settings"></i>
+                                        </div>
+                                        <h3 className="h5 mb-0">Fully Customizable</h3>
+                                    </div>
+
+                                    <div className="col-md-6 col-sm-6">
+                                        <div className="icon-7x mb-4">
+                                            <i className="ti-headphone-alt"></i>
+                                        </div>
+                                        <h3 className="h5 mb-0">Friendly Support</h3>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         )
     }
 }
---
+++ src/components/Services.tsx
@@ -0,0 +1,75 @@
+import React, {useCallback} from "react";
+
+
+class Features extends React.Component<any, any>{
+    render() {
+        return (
+<div className="website-slider-item bg-transparent" id="services" data-navbar-slide="navbar-dark">
+    <div className="website-slider-item-inner">
+
+        <div className="container text-white">
+            <div className="row mb-7 text-center">
+                <div className="col-12 col-lg-9 mx-lg-auto">
+                    <h2 className="h1">Services</h2>
+                    <div className="divider bg-white mx-auto"></div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+            </div>
+            <div className="row my-n3 mx-sm-n3">
+
+                <div className="col-md-6 py-3 px-md-3">
+                    <div className="d-sm-flex">
+                        <div className="icon-7x flex-shrink-0 mb-3 mr-sm-5">
+                            <i className="ti-timer"></i>
+                        </div>
+                        <div>
+                            <h3 className="h4 mb-2">Launch Quickly</h3>
+                            <p>Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis condimentum eros sollicitudin.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 py-3 px-md-3">
+                    <div className="d-sm-flex">
+                        <div className="icon-7x flex-shrink-0 mb-3 mr-sm-5">
+                            <i className="ti-brush-alt"></i>
+                        </div>
+                        <div>
+                            <h3 className="h4 mb-2">Stylish Design</h3>
+                            <p>Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis condimentum eros sollicitudin.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 py-3 px-md-3">
+                    <div className="d-sm-flex">
+                        <div className="icon-7x flex-shrink-0 mb-3 mr-sm-5">
+                            <i className="ti-book"></i>
+                        </div>
+                        <div>
+                            <h3 className="h4 mb-2">Well Documented</h3>
+                            <p>Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis condimentum eros sollicitudin.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-6 py-3 px-md-3">
+                    <div className="d-sm-flex">
+                        <div className="icon-7x flex-shrink-0 mb-3 mr-sm-5">
+                            <i className="ti-settings"></i>
+                        </div>
+                        <div>
+                            <h3 className="h4 mb-2">Fully Customizable</h3>
+                            <p>Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis condimentum eros sollicitudin.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</div>
+        )
+    }
+}
