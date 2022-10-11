b080f84f3f43d6e460009fb2cf15a70e8c7ab5e5
lucilefreyssac <lucilefr@outlook.fr>
2022-10-11 15:17:23 +0200

    mise a jour

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -11,6 +11,28 @@ class Subscribe extends React.Component<any, any>{
                         <button type="button" className="btn btn-sm btn-icon btn-primary position-absolute top-0 right-0 mt-n3 mr-n3 zindex-1" data-dismiss="modal">
                             <i className="fas fa-times btn-icon-inner"></i>
                         </button>
+                        <div className="modal-body py-5 py-lg-8 px-5 px-lg-8 text-center">
+
+                            <h2 id="subscribeNewsletter" className="mb-4">Subscribe<br/> to our newsletter</h2>
+                            <p className="mb-7">Signup for our weekly newsletter to get the latest news, updates and
+                                amazing offers delivered directly in your inbox.</p>
+                            <div className="subscribe-form">
+                                <form id="sf" name="sf" action="include/subscribe.php" method="post">
+                                    <div className="form-process"></div>
+                                    <div className="form-group">
+                                        <input type="email" id="sf-email" name="sf-email"
+                                               placeholder="Enter Your Email Address" className="form-control required"/>
+                                    </div>
+                                    <input type="text" id="sf-botcheck" name="sf-botcheck" value=""
+                                           className="form-control d-none"/>
+                                        <button className="btn btn-primary btn-block" type="submit" id="sf-submit"
+                                                name="sf-submit">Notify Me
+                                        </button>
+                                </form>
+                                <div className="subscribe-form-result"></div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
