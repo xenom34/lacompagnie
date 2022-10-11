bcaa59600c8bb96a371988746f93c6f6a2a80af7
lucilefreyssac <lucilefr@outlook.fr>
2022-10-11 13:57:20 +0200

    mise a jour

---
+++ src/components/Solutions.tsx
@@ -0,0 +1,61 @@
+import React, {useCallback} from "react";
+
+
+class Solutions extends React.Component<any, any>{
+    render() {
+        return (
+            <div className={"Solutions"}>
+    <div class="website-slider-item bg-transparent" id="solutions" data-navbar-slide="navbar-dark">
+        <div class="website-slider-item-inner">
+
+            <div class="container text-white">
+                <div class="row mb-7 text-center">
+                    <div class="col-12 col-lg-9 mx-lg-auto">
+                        <h2>A complete marketing solution</h2>
+                        <div class="divider bg-white mx-auto"></div>
+                        <p>Mauris ut sem elementum, tristique velit sed, volutpat mauris. Ut interdum nibh sit amet
+                            imperdiet condimentum. Nulla sed tellus placerat, vehicula risus ac, sollicitudin quam.</p>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-lg-6 mb-8">
+                        <h4>We are</h4>
+                        <p class="mb-0">Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a
+                            nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis
+                            condimentum eros sollicitudin. Morbi eget lorem turpis. Maecenas id metus sapien. Aliquam et
+                            est turpis. Sed sit amet dui vel sapien sagittis pellentesque nec id sem.</p>
+                    </div>
+
+                    <div class="col-12 col-lg-6 mb-8">
+                        <h4>We practice</h4>
+                        <p class="mb-0">Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a
+                            nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis
+                            condimentum eros sollicitudin. Morbi eget lorem turpis. Maecenas id metus sapien. Aliquam et
+                            est turpis. Sed sit amet dui vel sapien sagittis pellentesque nec id sem.</p>
+                    </div>
+
+                    <div class="col-12 col-lg-6 mb-8 mb-lg-0">
+                        <h4>We deliver</h4>
+                        <p class="mb-0">Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a
+                            nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis
+                            condimentum eros sollicitudin. Morbi eget lorem turpis. Maecenas id metus sapien. Aliquam et
+                            est turpis. Sed sit amet dui vel sapien sagittis pellentesque nec id sem.</p>
+                    </div>
+
+                    <div class="col-12 col-lg-6">
+                        <h4>We create and innovate</h4>
+                        <p class="mb-0">Quisque ultrices non velit sit amet consectetur. Cras turpis dolor, facilisis a
+                            nibh non, ullamcorper facilisis mauris. Nulla rutrum arcu sed ligula malesuada, quis
+                            condimentum eros sollicitudin. Morbi eget lorem turpis. Maecenas id metus sapien. Aliquam et
+                            est turpis. Sed sit amet dui vel sapien sagittis pellentesque nec id sem.</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+            </div>
+    }
+}
