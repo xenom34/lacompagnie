8c8958d524aabeab26b1abe97ce7213c885137c4
lucilefreyssac <lucilefr@outlook.fr>
2022-10-20 10:01:28 +0200

    Mise en place du mode sombre. /!\ problème de changement de CSS entre dark & light

--- src/components/ModeNuit.tsx
+++ src/components/ModeNuit.tsx
@@ -1,46 +1,33 @@
 import React, { useState } from 'react'
 import DayNightToggle from 'react-day-and-night-toggle'
 
-const ModeNuit = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false)
-    return (
-        <DayNightToggle
-            onChange={() => setIsDarkMode(!isDarkMode)}
-            checked={isDarkMode}
-        />
-    )
-}
-class ModeNuitClass extends React.Component{
+class ModeNuit extends React.Component<any,any>{
 
     constructor(props:any){
         super(props);
         this.state = {
             isDarkMode : false
         }
+        this.changeColor.bind(this)
 
     }
 
-    componentDidMount() {
-        this.setState({
-            black:true
-        })
-    }
-
-    changeColor(){
-        let color = this.state;
-        this.setState({black: color})
+    changeColor= () =>{
+        let color = !this.state.isDarkMode;
+        this.setState({isDarkMode: color})
         return color
     }
 
-    render(){
+    render = () =>{
         let color = this.state;
         let btn_class = color ? "blackButton" : "whiteButton";
 
         return (
             <div>
-                <button className={btn_class} onClick={() => this.setState({isDarkMode :this.changeColor.bind(this)})}>
-                    Button
-                </button>
+                <DayNightToggle
+                    onChange={() => this.setState({isDarkMode :this.changeColor()})}
+                    checked={this.state.isDarkMode}
+                />
             </div>
 
         )
